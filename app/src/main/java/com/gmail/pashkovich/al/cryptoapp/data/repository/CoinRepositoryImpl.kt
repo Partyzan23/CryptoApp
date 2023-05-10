2b12474d33d3c94db6a9f7eb7a27c173acbfb009
Alex Pashkovich <al.pashkovich@gmail.com>
2023-05-10 10:50:55 +0200

    Add RefreshDataWorker.kt

--- app/src/main/java/com/gmail/pashkovich/al/cryptoapp/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/gmail/pashkovich/al/cryptoapp/data/repository/CoinRepositoryImpl.kt
@@ -4,9 +4,12 @@ import android.app.Application
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
+import androidx.work.ExistingWorkPolicy
+import androidx.work.WorkManager
 import com.gmail.pashkovich.al.cryptoapp.data.database.AppDatabase
 import com.gmail.pashkovich.al.cryptoapp.data.mapper.CoinMapper
 import com.gmail.pashkovich.al.cryptoapp.data.network.ApiFactory
+import com.gmail.pashkovich.al.cryptoapp.data.workers.RefreshDataWorker
 import com.gmail.pashkovich.al.cryptoapp.domain.CoinInfo
 import com.gmail.pashkovich.al.cryptoapp.domain.CoinRepository
 import kotlinx.coroutines.delay
@@ -18,8 +21,6 @@ class CoinRepositoryImpl(
     private val coinInfoDao = AppDatabase.getInstance(application).coinPriceInfoDao()
 
     private val mapper = CoinMapper()
-
-    private val apiService = ApiFactory.apiService
 
     override fun getCoinInfoList(): LiveData<List<CoinInfo>> {
         return coinInfoDao.getPriceList().map {
@@ -35,21 +36,12 @@ class CoinRepositoryImpl(
         }
     }
 
-    override suspend fun loadData() {
-        while (true) {
-            try {
-                val topCoins = apiService.getTopCoinsInfo(limit = 20)
-                val fSyms = mapper.mapNamesListToString(topCoins)
-                val jsonContainer = apiService.getFullPriceList(fSyms = fSyms)
-                val coinInfoListDto = mapper.mapJsonContainerToListCoinInfo(jsonContainer)
-                val dbModelList = coinInfoListDto.map {
-                    mapper.mapDtoToDbModel(it)
-                }
-                coinInfoDao.insertPriceList(dbModelList)
-            } catch (e: Exception) {
-            }
-            delay(10000)
-        }
-
+    override fun loadData() {
+        val workManager = WorkManager.getInstance(application)
+        workManager.enqueueUniqueWork(
+            RefreshDataWorker.NAME,
+            ExistingWorkPolicy.REPLACE,
+            RefreshDataWorker.makeRequest()
+        )
     }
 }
--- app/src/main/java/com/gmail/pashkovich/al/cryptoapp/domain/LoadDataUseCase.kt
+++ app/src/main/java/com/gmail/pashkovich/al/cryptoapp/domain/LoadDataUseCase.kt
@@ -3,5 +3,5 @@ package com.gmail.pashkovich.al.cryptoapp.domain
 class LoadDataUseCase(
     private val coinRepository: CoinRepository
 ) {
-   suspend operator fun invoke() = coinRepository.loadData()
+   operator fun invoke() = coinRepository.loadData()
 }
